f008295d5bdaa03167d4901c42e2da14d78ee9c7
parrrate <alisa@parrrate.com>
2025-12-04 13:41:11 +0000

    `try_stream` (rename)

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ impl<T, E> Co<T, E> {
     }
 }
 
-pub fn to_try_stream<T: Send, E: Send, F: Send + TryFuture<Output = Result<(), E>>>(
+pub fn try_stream<T: Send, E: Send, F: Send + TryFuture<Output = Result<(), E>>>(
     f: impl Send + FnOnce(Co<T, E>) -> F,
 ) -> impl Unpin + Send + Stream<Item = Result<T, E>> {
     Gen::new(async |co| {
